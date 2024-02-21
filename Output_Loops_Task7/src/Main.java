9ccd833f2efa75fe04a8009f1540868776d82120
caleb <steppcb@mail.uc.edu>
2024-02-21 14:09:09 -0500

    Initial commit

---
+++ Output_Loops_Task7/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        for(int i=0;i<5;i++) //looping the * to repeat itself 5 times
+            System.out.println("***** "); //pattern being used each time
+    }
+}
